b18c2660d1e3c78c2315719a0c3b49af69998af8
Ali Derraj <aderraj@c4r5p4.1337.ma>
2024-03-20 15:20:53 +0000

    2

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "push_swap.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_stack
+{
+    int    value;
+    struct s_stack   *next;
+} t_stack;
+
 void  add_node(t_stack **a, t_stack *new)
 {
   t_stack *last;
@@ -15,30 +24,52 @@ void  add_node(t_stack **a, t_stack *new)
   new->next = 0;
 }
 
+void create_stack(t_stack **head)
+{
+  t_stack *x = malloc(sizeof(t_stack));
+  t_stack *y = malloc(sizeof(t_stack));
+  t_stack *z = malloc(sizeof(t_stack));
+
+  *head = 0;
+  x->value = 2;
+  y->value = 1;
+  z->value = 3;
+
+  add_node(head, x);
+  add_node(head, y);
+  add_node(head, z);
+}
+
+void    rra(t_stack **a)
+{
+    t_stack *tmp;
+
+    tmp = *a;
+    while (tmp && tmp->next)
+    {
+      if (tmp->next->next == NULL)
+        break;
+      tmp = tmp->next;
+    }
+    tmp->next->next = (*a);
+    (*a) = tmp->next;
+    tmp->next = 0;
+}
+
 int main() {
-  t_stack *head, *tmp;
-  t_stack x;
-  t_stack y;
-  t_stack z;
 
-  head = 0;
-  x.value = 2;
-  y.value = 1;
-  z.value = 3;
+  t_stack *a,*tmp;
 
-  add_node(&head, &x);
-  add_node(&head, &y);
-  add_node(&head, &z);
-  tmp = head;
+  create_stack(&a);
+  tmp = a;
   while (tmp)
   {
     printf("[%d]\n", tmp->value);
     tmp = tmp->next;
   }
-  sa(&head);
-  tmp = head;
-  printf("\nswapped \n\n");
-
+  rra(&a);
+  printf("\n reverse rotation \n");
+  tmp = a;
   while (tmp)
   {
     printf("[%d]\n", tmp->value);
